2e27c0af41c569e58853aaef3f0361c0f7e615d2
TimurMahmudov <verloren454@gmail.com>
2026-09-21 23:19:53 +0300

    Create users tables

---
+++ db_models/users_models.py
@@ -0,0 +1,24 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String
+
+from connection_data.base import Base  # type: ignore
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    account: Mapped["UserAccounts"] = relationship(back_populates="user")
+
+
+class UserAccounts(Base):
+    __tablename__ = 'useraccounts'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id",
+                                                    ondelete="CASCADE"))
+    first_name: Mapped[str] = mapped_column(String(30))
+    last_name: Mapped[str] = mapped_column(String(30))
+
+    user: Mapped["Users"] = relationship(back_populates="account")
