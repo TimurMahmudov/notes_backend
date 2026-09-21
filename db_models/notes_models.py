69f62e128dbdfffd13e25e1dbfbb0926b63cf1bf
TimurMahmudov <verloren454@gmail.com>
2026-09-21 23:41:22 +0300

    Finished notes model

--- db_models/notes_models.py
+++ db_models/notes_models.py
@@ -1,6 +1,17 @@
-from sqlalchemy import Text, String
+from sqlalchemy import ForeignKey, Text, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from connection_data.base import Base
 
 
-class Notes
+class Notes(Base):
+    __tablename__ = "notes"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(Text)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id",
+                                                    ondelete="CASCADE"))
+
+    user: Mapped["Users"] = relationship(
+        back_populates="notes")  # type: ignore
